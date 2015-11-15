28c5af5ea5d36b1d5c1f5cd2b1abd502c25158ef
lordobai <lord_opanyo@yahoo.com>
2015-11-15 10:04:37 +0100

    new edit

--- index2.js
+++ index2.js
@@ -28,6 +28,51 @@ var main2 = function () {
     $('.dropdown-toggle').click( function () {
         $('.dropdown-menu').toggle();
     });
+	$('.arrow-next').click( function () {
+        var currentSlide = $('.active-slide');
+        var nextSlide = currentSlide.next('.slide');
+        
+        if(nextSlide.length == 0) {
+            nextSlide= $('.slide').first();
+        }
+        
+        currentSlide.fadeOut(600).removeClass('active-slide');
+        nextSlide.fadeIn(600).addClass('active-slide');
+        
+        var currentDot = $('.active-dot');
+        var nextDot= currentDot.next();
+        
+        if (nextDot.length == 0) {
+            nextDot= $('.dot').first();
+        }
+        
+        currentDot.removeClass('active-dot');
+        nextDot.addClass('active-dot');
+    });
+        
+    $('.arrow-prev').click( function () {
+        var currentSlide = $('.active-slide');
+        var nextSlide = currentSlide.prev('.slide');
+        
+        if(nextSlide.length == 0) {
+            nextSlide= $('.slide').last();
+        }
+        
+    currentSlide.fadeOut(600).removeClass('active-slide');
+        nextSlide.fadeIn(600).addClass('active-slide');
+        
+        var currentDot = $('.active-dot');
+        var nextDot= currentDot.prev();
+        
+        if (nextDot.length == 0) {
+            nextDot= $('.dot').last();
+        }
+        
+        currentDot.removeClass('active-dot');
+        nextDot.addClass('active-dot');
+        
+    });
+	
 };
 
 $(document).ready(main).ready(main2);
